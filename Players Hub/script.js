9f3724142ad63342b15268471dcb2ad6a5e18595
itsVikrant <90085077+itsVikrant@users.noreply.github.com>
2022-02-09 15:23:40 +0530

    Initial Commit

---
+++ Players Hub/script.js
@@ -0,0 +1,8 @@
+//alert("PlayerHub is gmae news and trends sharing site.");
+
+const subscribebutton = document.querySelector('.button-subscribe');
+const input = document.querySelector('.section-7-input');
+
+subscribebutton.addEventListener('click', function () {
+  input.style.display = "hidden";
+});
